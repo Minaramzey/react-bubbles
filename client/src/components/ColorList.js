86fd1a1e11f2d208ca452ebfe961c964c096be84
Mina Ramzey <52507627+Minaramzey@users.noreply.github.com>
2020-03-27 19:26:23 -0400

    Update add section

--- client/src/components/ColorList.js
+++ client/src/components/ColorList.js
@@ -9,6 +9,7 @@ const ColorList = ({ colors, updateColors }) => {
   console.log(colors);
   const [editing, setEditing] = useState(false);
   const [colorToEdit, setColorToEdit] = useState(initialColor);
+  const [colorToAdd, setColorToAdd] = useState(initialColor);
 
   const editColor = color => {
     setEditing(true);
@@ -51,9 +52,55 @@ const ColorList = ({ colors, updateColors }) => {
     });
   };
 
+  const addColor = event => {
+    event.preventDefault();
+    axiosWithAuth()
+    .post("http://localhost:5000/api/colors", colorToAdd)
+    .then (response => {
+      console.log("Color added:", response);
+
+      updateColors([...colors, colorToAdd]);
+
+    })
+    .catch (error => {
+      console.log("error add:", error);
+    })
+  }
+
+
+
   return (
     <div className="colors-wrap">
       <p>colors</p>
+      <div className="addColorDiv">
+        <form name="addColor">
+          <legend>add color</legend>
+              <label>
+                color name:
+                <input
+                  onChange={e =>
+                    setColorToAdd({ ...colorToAdd, color: e.target.value })
+                  }
+                  value={colorToAdd.color}
+                />
+              </label>
+              <label>
+                hex code:
+                <input
+                  onChange={e =>
+                    setColorToAdd({
+                      ...colorToAdd,
+                      code: { hex: e.target.value }
+                    })
+                  }
+                  value={colorToAdd.code.hex}
+                />
+              </label>
+              <div className="button-row">
+                <button onClick={addColor}>add</button>
+              </div>
+            </form>
+          </div>
       <ul>
         {colors.map(color => (
           <li key={color.color} onClick={() => editColor(color)}>
@@ -106,4 +153,4 @@ const ColorList = ({ colors, updateColors }) => {
   );
 };
 
-export default ColorList;
+export default ColorList; 
